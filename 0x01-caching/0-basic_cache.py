3c2924d9603b65c6e03ad110dd76a6bdb0dd2406
JohnKamaujk <johnjkkamau@gmail.com>
2024-01-30 18:46:53 +0300

    :white_check_mark:basic cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -13,8 +13,9 @@ class BaseCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        if key is None and item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         """Retrieve an item by key
