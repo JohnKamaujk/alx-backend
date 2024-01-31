b028f00905000077ada4d2e3d0d2c3bf52a02305
JohnKamaujk <johnjkkamau@gmail.com>
2024-01-31 19:57:10 +0300

    :white_check_mark:FIFO cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 """FIFO caching module.
 """
-
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """Represents an object that allows storing and
+    """Represents an cache mecanism that allows storing and
     retrieving items from a dictionary with a FIFO
-    removal mechanism when the limit is reached.
+    removal mechanism when limit is reached.
     """
     def __init__(self):
         """Initializes the cache.
         """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Adds an item in the cache.
