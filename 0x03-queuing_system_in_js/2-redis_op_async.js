5ff20bd37069dfb015e786334a221f968fd6beb3
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-04 13:55:46 +0300

    :white_check_mark:redis advanced operations

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -3,12 +3,12 @@ import { promisify } from 'util';
 
 const client = redis.createClient();
 
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
-});
-
 client.on('error', (err) => {
   console.error(`Redis client not connected to the server: ${err}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
 });
 
 const getAsync = promisify(client.get).bind(client);
